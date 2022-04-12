93ccaa7031e9fdbe96b0487a7c4721b2b2017832
Sammy Gelman <sammygelman@gmail.com>
2022-04-12 15:34:31 +0300

    still one mapping bug to fix

--- mushroom_data.py
+++ mushroom_data.py
@@ -1,6 +1,8 @@
+#!/usr/bin/env python3
 import numpy as np
-import matplotlib.pyplot
+import matplotlib.pyplot as plt
 import cv2
+import pickle
 
 def spore_mushrooms(spore_table, n_spores, max_radius):
     length = len(spore_table)
@@ -10,15 +12,13 @@ def spore_mushrooms(spore_table, n_spores, max_radius):
     len_width = np.linspace(0, width - 1, width).astype(int)
 
     spored = 0
-
+    holder = 0.1
     idx_radius = (max_radius - 1)
 
     while spored < n_spores:
         spore = True
         prop_x = np.random.choice(len_range)
         prop_y = np.random.choice(len_width)
-
-        holder = 0.1
 
         x_range = np.linspace(prop_x - idx_radius,
                               prop_x + idx_radius,
@@ -34,7 +34,7 @@ def spore_mushrooms(spore_table, n_spores, max_radius):
                 if (i and j) >= 0 and i < length and j < width:
                     if spore_table[i,j] != 0:
                         spore = False
-                        break()
+                        break
         if spore:
             for i in x_range:
                 for j in y_range:
@@ -46,33 +46,44 @@ def spore_mushrooms(spore_table, n_spores, max_radius):
     spore_table -= holder
     return spore_table
 
-
-def map_spores(growing_table, spore_table, spore_length, spore_width):
-   for i in range(spore_length):
-       for j in range(spore_width):
-           growing_table[i+max_radius,j+max_radius] = spore_table[i,j]
+def map_spores(growing_table, spore_table, spore_length, spore_width, max_radius):
+    for i in range(spore_length):
+        for j in range(spore_width):
+            growing_table[i+max_radius,j+max_radius] = spore_table[i,j]
     return growing_table
 
 def grow_mushrooms(growing_table, max_radius):
-    output = np.zeros((len(growing_table), len(growing_table[0])))
-    for i in growing_table:
-        for j in growing_table:
+    length = len(growing_table)
+    width = len(growing_table[0])
+    output = np.zeros((length, width))
+    for i in range(length):
+        for j in range(width):
             if growing_table[i,j] == 1:
-                r = round(np.random.random()*(max_radius - 1) + 1).astype(int)
+                r = round(np.random.random()*(max_radius - 1) + 1)
                 cv2.circle(output,(i,j),r,1,-1)
     return output
 
-def generate_data(max_radius, n_mushrooms, length, width, n_samples):
-    mushroom_data_chart = np.array()
-    growing_table = np.zeros((length,width))
-
+def generate_data(max_radius, max_mushrooms, length, width, n_samples):
+    bank = []
     spore_length = length - max_radius
     spore_width = width - max_radius
 
     for i in range(n_samples):
         spore_table = np.zeros((spore_length, spore_width))
-        n_spores = round(n_mushrooms*np.random.random()).astype(int)
+        growing_table = np.zeros((length,width))
+        n_spores = round(max_mushrooms*np.random.random())
         spore_mushrooms(spore_table, n_spores, max_radius)
 
-        map_spores(growing_table, spore_table, spore_length, spore_width)
+        map_spores(growing_table,
+                   spore_table,
+                   spore_length,
+                   spore_width,
+                   max_radius)
+
+        bank.append(grow_mushrooms(growing_table, max_radius))
+
+    with open('mushrooms.pkl', 'wb') as f:
+        pickle.dump(bank, f)
+
+generate_data(10, 5, 100, 100,5)
 
